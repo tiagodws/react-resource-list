0608700b2ca154530156ace3157a70b265b0d2f5
Tiago Dionesto Willrich da Silva <tiagodws@gmail.com>
2018-03-11 01:30:13 -0300

    improvements

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -12,6 +12,7 @@ export default function(state = {}, { type, payload }) {
             return { ...state, error: undefined };
         }
         case AUTH_REQUEST_FAIL: {
+            if (!payload || !payload.response) return { error: "It was not possible to sign-in right now. Please try again later" };
             const { status, data } = payload.response;
 
             if (status == 401) return { error: "Wrong username or password" };
